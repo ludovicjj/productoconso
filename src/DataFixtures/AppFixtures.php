e1eae265932be8f40a2218864e0b3054a5d137b0
ludovicjj <jahanlud@gmail.com>
2020-10-30 16:21:17 +0100

    fixtures: create one User(producer)

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -2,12 +2,24 @@
 
 namespace App\DataFixtures;
 
+use App\Entity\Producer;
 use App\Entity\Product;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 
 class AppFixtures extends Fixture
 {
+    /**
+     * @var UserPasswordEncoderInterface $passwordEncoder
+     */
+    private $passwordEncoder;
+
+    public function __construct(UserPasswordEncoderInterface $passwordEncoder)
+    {
+        $this->passwordEncoder = $passwordEncoder;
+    }
+
     public function load(ObjectManager $manager)
     {
         for ($i = 0; $i < 20; $i++) {
@@ -15,6 +27,14 @@ class AppFixtures extends Fixture
             $product->setName('product ' . $i);
             $manager->persist($product);
         }
+
+        $user = new Producer();
+        $user->setFirstName("John");
+        $user->setLastName("Doe");
+        $user->setEmail("johndoe@email.fr");
+        $user->setPassword($this->passwordEncoder->encodePassword($user, '123456'));
+        $manager->persist($user);
+
 
         $manager->flush();
     }
