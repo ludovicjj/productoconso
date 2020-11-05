b6683f9da08d88dd35ca2e97050bfb62cdbf58f0
ludovicjj <jahanlud@gmail.com>
2020-11-05 16:11:30 +0100

    refactor: login test

--- tests/EndToEnd/Security/LoginTest.php
+++ tests/EndToEnd/Security/LoginTest.php
@@ -1,7 +1,8 @@
 <?php
 
-namespace App\Tests\EndToEnd;
+namespace App\Tests\EndToEnd\Security;
 
+use App\Tests\EndToEnd\EndToEndIntegrationTestTrait;
 use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
@@ -13,7 +14,7 @@ class LoginTest extends WebTestCase
 
     public function testLoginSuccess(): void
     {
-        $this->loadFixture(__DIR__ . "/../fixtures/CreateUser.yml");
+        $this->loadFixture(__DIR__ . "/../../fixtures/CreateUser.yml");
 
         /** @var RouterInterface $router */
         $router = $this->client->getContainer()->get("router");
@@ -30,61 +31,57 @@ class LoginTest extends WebTestCase
 
     public function testLoginInvalidEmail(): void
     {
-        $client = static::createClient();
-        $this->loadFixture(__DIR__ . "/../fixtures/CreateUser.yml");
+        $this->loadFixture(__DIR__ . "/../../fixtures/CreateUser.yml");
 
         /** @var RouterInterface $router */
-        $router = $client->getContainer()->get("router");
+        $router = $this->client->getContainer()->get("router");
 
-        $crawler = $client->request(Request::METHOD_GET, $router->generate("security_login"));
+        $crawler = $this->client->request(Request::METHOD_GET, $router->generate("security_login"));
         $form = $crawler->filter("form[name=login_form]")->form([
             "email" => "fail@email.fr",
             "password" => "123456"
         ]);
 
-        $client->submit($form);
+        $this->client->submit($form);
         $this->assertResponseStatusCodeSame(Response::HTTP_FOUND);
-        $client->followRedirect();
+        $this->client->followRedirect();
         $this->assertSelectorTextContains("div.alert-danger", 'Cette adresse email n\'existe pas.');
     }
 
     public function testLoginInvalidPassword(): void
     {
-        $client = static::createClient();
-        $this->loadFixture(__DIR__ . "/../fixtures/CreateUser.yml");
+        $this->loadFixture(__DIR__ . "/../../fixtures/CreateUser.yml");
 
         /** @var RouterInterface $router */
-        $router = $client->getContainer()->get("router");
+        $router = $this->client->getContainer()->get("router");
 
-        $crawler = $client->request(Request::METHOD_GET, $router->generate("security_login"));
+        $crawler = $this->client->request(Request::METHOD_GET, $router->generate("security_login"));
         $form = $crawler->filter("form[name=login_form]")->form([
             "email" => "johndoe@email.fr",
             "password" => "fail"
         ]);
 
-        $client->submit($form);
+        $this->client->submit($form);
         $this->assertResponseStatusCodeSame(Response::HTTP_FOUND);
-        $client->followRedirect();
+        $this->client->followRedirect();
         $this->assertSelectorTextContains("div.alert-danger", 'Identifiants invalides.');
     }
 
     public function testLoginInvalidCsrfToken(): void
     {
-        $client = static::createClient();
-
         /** @var RouterInterface $router */
-        $router = $client->getContainer()->get("router");
+        $router = $this->client->getContainer()->get("router");
 
-        $crawler = $client->request(Request::METHOD_GET, $router->generate("security_login"));
+        $crawler = $this->client->request(Request::METHOD_GET, $router->generate("security_login"));
         $form = $crawler->filter("form[name=login_form]")->form([
             "email" => "johndoe@email.fr",
             "password" => "123456",
             "_csrf_token" => "fail"
         ]);
 
-        $client->submit($form);
+        $this->client->submit($form);
         $this->assertResponseStatusCodeSame(Response::HTTP_FOUND);
-        $client->followRedirect();
+        $this->client->followRedirect();
         $this->assertSelectorTextContains("div.alert-danger", 'Invalid CSRF token.');
     }
 }
