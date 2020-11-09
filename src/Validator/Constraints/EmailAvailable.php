da622f889c4235c36a49f17fd2c2b2f79f246f07
ludovicjj <jahanlud@gmail.com>
2020-11-09 11:50:43 +0100

    Custom constraints to email field

---
+++ src/Validator/Constraints/EmailAvailable.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Validator\Constraints;
+
+use Symfony\Component\Validator\Constraint;
+
+/**
+ * Class EmailAvailable
+ * @package App\Validator\Constraints
+ * @Annotation
+ */
+class EmailAvailable extends Constraint
+{
+    /** @var string $message */
+    public $message = "Cette adresse email est déjà utilisé";
+
+    public function getTargets()
+    {
+        return self::CLASS_CONSTRAINT;
+    }
+}
