cee05f2dddec09e6ca637da5fe9aaa486b197f80
ludovicjj <jahanlud@gmail.com>
2020-11-20 13:37:35 +0100

    add findUserByEmailExcludeCurrentUserEmail

--- src/Repository/UserRepository.php
+++ src/Repository/UserRepository.php
@@ -31,4 +31,26 @@ class UserRepository extends ServiceEntityRepository
             ->getQuery()
             ->getOneOrNullResult();
     }
+
+    /**
+     * @param string $email
+     * @param string $excludedEmail
+     * @return User|null
+     * @throws NonUniqueResultException
+     */
+    public function findUserByEmailExcludeCurrentUserEmail(string $email, string $excludedEmail): ?User
+    {
+        $qb =  $this->getEntityManager()->createQueryBuilder();
+
+        return $qb->select('u')
+            ->from('App\Entity\User', 'u')
+            ->where(
+                $qb->expr()->notLike("u.email", "?2"),
+                $qb->expr()->like("u.email", "?1")
+            )
+            ->setParameter(1, $email)
+            ->setParameter(2, $excludedEmail)
+            ->getQuery()
+            ->getOneOrNullResult();
+    }
 }
