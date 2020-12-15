6108583606f2dc1f87b68a6113985adfaa207217
ludovicjj <jahanlud@gmail.com>
2020-12-15 11:17:08 +0100

    Embeddable Image and Price entity

---
+++ src/Entity/Image.php
@@ -0,0 +1,35 @@
+<?php
+
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Class Image
+ * @package App\Entity
+ * @ORM\Embeddable
+ */
+class Image
+{
+    /**
+     * @ORM\Column(type="string", nullable=true)
+     */
+    private $path = null;
+
+    /**
+     * @return string|null
+     */
+    public function getPath(): ?string
+    {
+        return $this->path;
+    }
+
+    /**
+     * @param string|null $path
+     */
+    public function setPath(?string $path): void
+    {
+        $this->path = $path;
+    }
+}
